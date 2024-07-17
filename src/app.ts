84aeb719e0a75d3bcb90f713fca865af0ca9b64b
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-07-17 20:33:18 +0545

    Implementing route

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import express from 'express';
 import config from 'config';
 import connectToDB from './utils/connectToDB';
 import log from './utils/logger';
+import router from './routes';
 
 const app = express();
+
+app.use(router);
 
 const port = config.get('port');
 
