bf7f853a494134869a9a221da7650b382163889d
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-07-19 21:26:19 +0545

    Validating the user using middleware

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import validateResource from '../middleware/validateResource';
+import { createUserSchema } from '../schema/userSchema';
+import { createUserHandler } from '../controller/userController';
 
 const router = express.Router();
 
-router.post('api/users', (req, res) => {
-  res.sendStatus(200);
-});
+router.post('api/users', validateResource(createUserSchema), createUserHandler);
 
 export default router;
