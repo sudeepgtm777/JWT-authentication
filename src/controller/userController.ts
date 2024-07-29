7eb376c07a3ae87ddab66f33461c2a1d898ca3a9
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-07-29 22:17:18 +0545

    Using simple Email format

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { createUserInput } from '../schema/userSchema';
 import { createUser } from '../service/userService';
+import sendEmail from '../utils/mailer';
 
 export async function createUserHandler(
   req: Request<{}, {}, createUserInput>,
@@ -10,6 +11,13 @@ export async function createUserHandler(
 
   try {
     const user = await createUser(body);
+
+    await sendEmail({
+      from: 'test@example.com',
+      to: user.email,
+      subject: 'Please verify your account!!',
+      text: `Verification code ${user.verificationCode}. Id: ${user._id}`
+    });
 
     return res.send('User successfully created!!');
   } catch (err: any) {
